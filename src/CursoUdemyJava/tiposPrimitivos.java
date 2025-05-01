df9d48091b4a510c2c4e69a3f9d10a7116cf40d4
carlosprado0 <ibroonkzz@gmail.com>
2025-05-01 09:11:39 -0300

    CursoJava

---
+++ src/CursoUdemyJava/tiposPrimitivos.java
@@ -0,0 +1,18 @@
+package CursoUdemyJava;
+
+public class tiposPrimitivos {
+    public static void main(String[] args) {
+        int idade = 25;
+        double altura = 1.65; // a diferença para o float é que o double recebe números maiores
+        char genero = 'M'; // recebe apenas com aspas simples e um caractere
+        boolean ehVerdade = false; // ou true
+        String nome = "João";
+        long cpf = 12345678999L; //é um int soq maior, porem nao pode comecar com 0
+
+        System.out.println("Idade " + idade);
+        System.out.println("Altura " + altura);
+        System.out.println("Genero " + genero);
+        System.out.println("É verdade? " + ehVerdade);
+        System.out.println("Seu nome é? " + nome);
+    }
+}
